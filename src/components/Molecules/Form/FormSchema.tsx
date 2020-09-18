31096a0707c7feffb317b74b377c51db1bf8ce88
jumper2210 <47790273+jumper2210@users.noreply.github.com>
2020-09-18 11:03:37 +0200

    recaptcha and lazyload: added

--- src/components/Molecules/Form/FormSchema.tsx
+++ src/components/Molecules/Form/FormSchema.tsx
@@ -1,76 +1,104 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Button from "../../atoms/CustomButton/CustomButton"
 import FormInput from "./FormInput"
-import { Formik, Form, Field } from "formik"
+import { Formik, Form, Field, FormikHelpers } from "formik"
 import * as Yup from "yup"
 import styled from "styled-components"
+import Recaptcha from "react-recaptcha"
+import LazyLoad from "react-lazyload"
 
 const StyledButton = styled(Button)`
-  width: 100%;
-  margin: 1.2rem 0 2.5rem;
-  border-radius: 3px;
+    width: 100%;
+    margin: 1.2rem 0 2.5rem;
+    border-radius: 3px;
 `
-
 interface Data {
   [key: string]: string
 }
-
 interface FormValues {
   name: string
   email: string
   message: string
 }
-
 const ContactSchema = Yup.object().shape({
-  name: Yup.string().required("Your name is required!"),
+  name: Yup.string().required("Your name is required!"),
   email: Yup.string()
-    .email("Email address is invalid!")
-    .required("Email address is required!"),
+    .email("Email address is invalid!")
+    .required("Email address is required!"),
   message: Yup.string()
-    .min(6, "Message is too short!")
-    .required("Message is required!"),
+    .min(6, "Message is too short!")
+    .required("Message is required!"),
 })
-
 const initialValues: FormValues = {
   name: "",
   email: "",
   message: "",
 }
-
 const encode = (data: Data) => {
   return Object.keys(data)
     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&")
 }
-
 const FormSchema = () => {
+  const [token, setToken] = useState("")
+  const [submitBtn, setSubmitBtn] = useState({
+    content: "Send message",
+    color: "violet",
+  })
+  const clearButton = () => {
+    setSubmitBtn({ content: "Send message", color: "violet" })
+  }
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src = "https://www.google.com/recaptcha/api.js"
+    script.async = true
+    script.defer = true
+    document.body.appendChild(script)
+  }, [])
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={ContactSchema}
-      onSubmit={(values, actions) => {
-        const sendMessage = async () => {
-          try {
-            fetch("/", {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-              },
-              body: encode({ "form-name": "contact-form", ...values }),
-            })
-              .then(() => {
-                alert("Success")
-                actions.resetForm()
+      onSubmit={(
+        values,
+        { setSubmitting, resetForm }: FormikHelpers<FormValues>
+      ) => {
+        if (token) {
+          const sendMessage = async () => {
+            try {
+              await fetch("/", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/x-www-form-urlencoded",
+                },
+                body: encode({
+                  "form-name": "contact-form",
+                  ...values,
+                  "g-recaptcha-response": token,
+                }),
               })
-              .catch(() => {
-                alert("Error")
+              setSubmitting(false)
+              setSubmitBtn({
+                content: "All good! I'll respond as soon as possible!",
+                color: "bluViolet",
               })
-              .finally(() => actions.setSubmitting(false))
-          } catch (err) {
-            console.log(err)
+              resetForm()
+              setTimeout(clearButton, 2500)
+            } catch (err) {
+              setSubmitting(false)
+              setSubmitBtn({ content: "Something went wrong!", color: "red" })
+              setTimeout(clearButton, 1500)
+            }
           }
+          sendMessage()
+        } else {
+          setSubmitting(false)
+          setSubmitBtn({
+            content: "You have to verify reCAPTCHA first!",
+            color: "grey",
+          })
+          setTimeout(clearButton, 1000)
         }
-        sendMessage()
       }}
     >
       {({
@@ -80,18 +108,17 @@ const FormSchema = () => {
         handleChange,
         handleBlur,
         handleSubmit,
+        isSubmitting,
       }) => (
         <Form
           onSubmit={handleSubmit}
-          method="POST"
-          autoComplete="off"
-          data-netlify={true}
+          data-netlify="true"
           name="contact-form"
           data-netlify-honeypot="bot-field"
           data-netlify-recaptcha="true"
         >
           <Field type="hidden" name="form-name" />
-          <Field type="hidden" name="bot-field" />
+          <Field type="hidden" name="bot-field" />           
           <FormInput
             id="name"
             label="name"
@@ -121,11 +148,31 @@ const FormSchema = () => {
             errors={errors.message}
           />
           <div data-netlify-recaptcha="true"></div>
-          <StyledButton type="submit">Send a message</StyledButton>
+          <LazyLoad height={70} offset={280}>
+            <Recaptcha
+              sitekey={process.env.SITE_RECAPTCHA_KEY}
+              render="explicit"
+              theme="dark"
+              verifyCallback={response => {
+                setToken(response)
+              }}
+              onloadCallback={() => {
+                clearButton()
+              }}
+            />
+          </LazyLoad>
+            
+          <StyledButton
+            color={submitBtn.color}
+            disabled={isSubmitting || submitBtn.color !== "violet"}
+            isSubmitting={isSubmitting}
+            type="submit"
+          >
+            {!isSubmitting && submitBtn.content}    
+          </StyledButton>
         </Form>
       )}
     </Formik>
   )
 }
-
 export default FormSchema
--- src/components/atoms/CustomButton/CustomButton.ts
+++ src/components/atoms/CustomButton/CustomButton.ts
@@ -1,7 +1,10 @@
-import styled from "styled-components"
+// @ts-nocheck
+import styled, { css } from "styled-components"
 
 interface Props {
   readonly isSubmitting?: boolean
+  readonly disabled?: boolean
+  readonly color?: string
 }
 
 const Button = styled.button<Props>`
@@ -13,6 +16,32 @@ const Button = styled.button<Props>`
   font-size: ${({ theme }) => theme.fontSize.s};
   background: ${({ theme }) => theme.colors.color_secondary};
   cursor: pointer;
+    ${({ color }) =>
+      color === "violet" &&
+      css`
+        border-color: ${({ theme }) => theme.colors.accent};
+        background: ${({ theme }) => theme.colors.accent};
+      `}
+   ${({ color }) =>
+     color === "bluViolet" &&
+     css`
+       border-color: ${({ theme }) => theme.colors.color_secondary} !important;
+       background: ${({ theme }) => theme.colors.color_secondary} !important;
+       &:hover {
+         border-color: ${({ theme }) => theme.colors.color_secondary};
+         background: ${({ theme }) => theme.colors.color_secondary};
+       }
+     `}
+  ${({ color }) =>
+    color === "grey" &&
+    css`
+      border-color: ${({ theme }) => theme.colors.grey} !important;
+      background: ${({ theme }) => theme.colors.grey} !important;
+      &:hover {
+        border-color: ${({ theme }) => theme.colors.grey};
+        background: ${({ theme }) => theme.colors.grey};
+      }
+    `}
 `
 
 export default Button
